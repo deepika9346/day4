f726f326da6d39647c16a02cf3d8403343a4f690
deepika9346 <deepika77020@gmail.com>
2026-02-07 15:54:51 +0530

    Add files via upload

---
+++ primitive1.java
@@ -0,0 +1,20 @@
+class primitive1 {
+       public static void main(String[] args) {
+      String var1 = "deepika";
+      byte var2 = 19;
+      byte var3 = 95;
+      char var4 = 'A';
+      System.out.println("str:" + var1);
+      System.out.println("int:" + var2);
+      System.out.println("int:" + var3);
+      System.out.println("char:" + var4);
+      if (var3 >= 45) 
+      {
+         System.out.println("pass");
+      }
+      else {
+         System.out.println("fail");
+      }
+              System.out.println("pass"); 
+   }
+}
