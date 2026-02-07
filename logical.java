ce66a749e8301d4e0a1bd575dc5235b74a1f307c
deepika9346 <deepika77020@gmail.com>
2026-02-07 15:55:09 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,13 @@
+class logical{
+    public static void main(String[] args) {
+        int num=-25;
+        if(num>0 && num%2==0)
+        {
+            System.out.println("even and positive");
+        }
+        else
+        {
+            System.out.println("not both");
+        }
+    }
+}
