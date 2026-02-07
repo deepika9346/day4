0d4cfb4a7a24fbd3391529151d14695d6aa0b79a
deepika9346 <deepika77020@gmail.com>
2026-02-07 15:52:47 +0530

    Add files via upload

---
+++ totavg.java
@@ -0,0 +1,18 @@
+class totavg {
+    public static void main(String[] args) {
+        String s1 = "Deepika";
+        String s2 = "Bunty";
+        String s3 = "Chintu";
+        int a = 90;
+        int b = 93;
+        int c = 89;
+        int sum = a + b + c;
+        float avg = (a + b + c) / 3.0f;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println("Total Marks: " + sum);
+        System.out.println("Average Marks: " + avg);
+    }
+}
+
