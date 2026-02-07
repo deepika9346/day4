a5070b35337550de3d38e06b53b83ee705dab67b
deepika9346 <deepika77020@gmail.com>
2026-02-07 15:55:21 +0530

    Add files via upload

---
+++ arthimetic.java
@@ -0,0 +1,10 @@
+public class arthimetic {
+    public static void main(String[] args){
+    int a=10,b=20;
+    System.out.println("sum:"+(a+b));
+     System.out.println("sub:"+(a-b));
+      System.out.println("pro:"+(a*b));
+       System.out.println("mod:"+(a%b));
+        System.out.println("div:"+(a/b));
+}
+}
